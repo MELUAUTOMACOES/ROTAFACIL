f24e081fcd21fc8aca41cade5a93ee1af144511d
Lucas <meluautomacoes@gmail.com>
2026-04-11 13:47:02 -0300

    Scroll em mobile nos cards de forms

--- client/src/components/forms/AppointmentForm.tsx
+++ client/src/components/forms/AppointmentForm.tsx
@@ -601,19 +601,21 @@ export default function AppointmentForm({
   }, [appointment]);
 
   return (
-    <div className="flex flex-col flex-1 w-full h-full min-h-0">
-      <div className="p-6 pb-4 border-b shrink-0">
-        <DialogHeader>
-          <DialogTitle className="flex items-center gap-2">
-            <Calendar className="h-5 w-5" />
-            {appointment ? "Editar Agendamento" : "Novo Agendamento"}
-          </DialogTitle>
-        </DialogHeader>
-      </div>
+    <>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col flex-1 min-h-0 w-full">
+        <div className="p-5 sm:p-6 pb-4 border-b shrink-0 bg-white z-10 sticky top-0">
+          <DialogHeader>
+            <DialogTitle className="flex items-center gap-2 text-lg sm:text-xl">
+              <Calendar className="h-5 w-5 shrink-0" />
+              <span className="truncate">
+                 {appointment ? "Editar Agendamento" : "Novo Agendamento"}
+              </span>
+            </DialogTitle>
+          </DialogHeader>
+        </div>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col flex-1 min-h-0">
-          <div className="flex-1 overflow-y-auto p-6 space-y-6">
+        <div className="flex-1 overflow-y-auto p-4 sm:p-6 space-y-5 flex flex-col min-h-0">
 
             {/* Alerta de Romaneio Confirmado/Finalizado */}
             {isInConfirmedRoute && (
@@ -1306,6 +1308,6 @@ export default function AppointmentForm({
           )}
         </DialogContent>
       </Dialog>
-    </div>
+    </>
   );
 }
